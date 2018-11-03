b59a680b8cf343db44242aec7e543f793b256c5e
TheRandomLabs <therandomlabsinc@gmail.com>
2018-11-03 15:10:33 +1100

    Fix shadowJar

--- src/main/java/com/therandomlabs/vanilladeathchest/VanillaDeathChest.java
+++ src/main/java/com/therandomlabs/vanilladeathchest/VanillaDeathChest.java
@@ -1,6 +1,11 @@
 package com.therandomlabs.vanilladeathchest;
 
+import java.security.MessageDigest;
+import java.security.cert.Certificate;
+import java.util.ArrayList;
+import java.util.List;
 import com.therandomlabs.vanilladeathchest.config.VDCConfig;
+import com.therandomlabs.vanilladeathchest.util.CertificateHelper;
 import net.minecraft.crash.CrashReport;
 import net.minecraft.crash.ReportedException;
 import org.apache.logging.log4j.LogManager;
@@ -11,10 +16,15 @@ import org.spongepowered.asm.mixin.Mixins;
 
 public final class VanillaDeathChest implements InitializationListener {
 	public static final String MOD_ID = "vanilladeathchest";
+	public static final String CERTIFICATE_FINGERPRINT = "@FINGERPRINT@";
 	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialization() {
+		if(!verifyFingerprint()) {
+			LOGGER.error("Invalid fingerprint detected!");
+		}
+
 		MixinBootstrap.init();
 		Mixins.addConfiguration("mixins." + MOD_ID + ".json");
 
@@ -23,5 +33,19 @@ public final class VanillaDeathChest implements InitializationListener {
 
 	public static void crashReport(String message, Throwable throwable) {
 		throw new ReportedException(new CrashReport(message, throwable));
+	}
+
+	private static boolean verifyFingerprint() {
+		final List<String> fingerprints = CertificateHelper.getFingerprints(
+				VanillaDeathChest.class.getProtectionDomain().getCodeSource().getCertificates()
+		);
+
+		for(String fingerprint : fingerprints) {
+			if(CERTIFICATE_FINGERPRINT.equals(fingerprint)) {
+				return true;
+			}
+		}
+
+		return false;
 	}
 }

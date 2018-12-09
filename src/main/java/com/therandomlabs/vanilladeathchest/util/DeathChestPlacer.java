148784145ebc185bc9973173c8fe38064429dbea
TheRandomLabs <therandomlabsinc@gmail.com>
2018-12-10 10:41:12 +1100

    Random shulker box colors

--- src/main/java/com/therandomlabs/vanilladeathchest/util/DeathChestPlacer.java
+++ src/main/java/com/therandomlabs/vanilladeathchest/util/DeathChestPlacer.java
@@ -2,6 +2,7 @@ package com.therandomlabs.vanilladeathchest.util;
 
 import java.lang.ref.WeakReference;
 import java.util.List;
+import java.util.Random;
 import java.util.UUID;
 import com.mojang.authlib.GameProfile;
 import com.therandomlabs.vanilladeathchest.api.deathchest.DeathChestManager;
@@ -13,6 +14,7 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
+import net.minecraft.item.EnumDyeColor;
 import net.minecraft.state.properties.ChestType;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.tileentity.TileEntityLockableLoot;
@@ -25,8 +27,11 @@ public final class DeathChestPlacer {
 	public enum DeathChestType {
 		SINGLE_ONLY,
 		SINGLE_OR_DOUBLE,
-		SHULKER_BOX
+		SHULKER_BOX,
+		RANDOM_SHULKER_BOX_COLOR
 	}
+
+	private static final Random random = new Random();
 
 	private final WeakReference<World> world;
 	private final WeakReference<EntityPlayer> player;
@@ -94,6 +99,8 @@ public final class DeathChestPlacer {
 
 		if(type == DeathChestType.SHULKER_BOX) {
 			block = BlockShulkerBox.getBlockByColor(VDCConfig.spawning.shulkerBoxColor);
+		} else if(type == DeathChestType.RANDOM_SHULKER_BOX_COLOR) {
+			block = BlockShulkerBox.getBlockByColor(EnumDyeColor.byId(random.nextInt(16)));
 		} else {
 			block = Blocks.CHEST;
 		}

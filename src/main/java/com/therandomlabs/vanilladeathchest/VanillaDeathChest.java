3ea0fc74bb834dedf2acf9e2d47d02798898c010
TheRandomLabs <therandomlabsinc@gmail.com>
2018-12-27 12:40:10 +1100

    Fix

--- src/main/java/com/therandomlabs/vanilladeathchest/VanillaDeathChest.java
+++ src/main/java/com/therandomlabs/vanilladeathchest/VanillaDeathChest.java
@@ -5,13 +5,15 @@ import com.therandomlabs.vanilladeathchest.config.VDCConfig;
 import com.therandomlabs.vanilladeathchest.util.CertificateHelper;
 import net.minecraft.crash.CrashReport;
 import net.minecraft.crash.ReportedException;
+import net.minecraft.world.GameRules;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.dimdev.rift.listener.ItemAdder;
 import org.dimdev.riftloader.listener.InitializationListener;
 import org.spongepowered.asm.launch.MixinBootstrap;
 import org.spongepowered.asm.mixin.Mixins;
 
-public final class VanillaDeathChest implements InitializationListener {
+public final class VanillaDeathChest implements InitializationListener, ItemAdder {
 	public static final String MOD_ID = "vanilladeathchest";
 	public static final String CERTIFICATE_FINGERPRINT = "@FINGERPRINT@";
 
@@ -27,6 +29,21 @@ public final class VanillaDeathChest implements InitializationListener {
 		Mixins.addConfiguration("mixins." + MOD_ID + ".json");
 
 		VDCConfig.reload();
+	}
+
+	//Registering the gamerule in onInitialization apparently causes MinecraftServer to initialize
+	//too early
+	@Override
+	public void registerItems() {
+		if(!VDCConfig.misc.gameRuleName.isEmpty()) {
+			GameRules.getDefinitions().put(
+					VDCConfig.misc.gameRuleName,
+					new GameRules.ValueDefinition(
+							Boolean.toString(VDCConfig.misc.gameRuleDefaultValue),
+							GameRules.ValueType.BOOLEAN_VALUE
+					)
+			);
+		}
 	}
 
 	public static void crashReport(String message, Throwable throwable) {

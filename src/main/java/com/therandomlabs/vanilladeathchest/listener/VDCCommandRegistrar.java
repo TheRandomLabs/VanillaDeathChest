76679c72b995c9be3bf1dcc0995bedb195660d4e
TheRandomLabs <therandomlabsinc@gmail.com>
2019-01-23 18:01:02 +1100

    Minor change

--- src/main/java/com/therandomlabs/vanilladeathchest/listener/VDCCommandRegistrar.java
+++ src/main/java/com/therandomlabs/vanilladeathchest/listener/VDCCommandRegistrar.java
@@ -1,7 +1,7 @@
 package com.therandomlabs.vanilladeathchest.listener;
 
 import com.mojang.brigadier.CommandDispatcher;
-import com.therandomlabs.vanilladeathchest.command.CommandVDCReload;
+import com.therandomlabs.vanilladeathchest.command.VDCReloadCommand;
 import com.therandomlabs.vanilladeathchest.config.VDCConfig;
 import net.minecraft.command.CommandSource;
 import org.dimdev.rift.listener.CommandAdder;
@@ -10,7 +10,7 @@ public class VDCCommandRegistrar implements CommandAdder {
 	@Override
 	public void registerCommands(CommandDispatcher<CommandSource> dispatcher) {
 		if(VDCConfig.misc.vdcreload) {
-			CommandVDCReload.register(dispatcher);
+			VDCReloadCommand.register(dispatcher);
 		}
 	}
 }

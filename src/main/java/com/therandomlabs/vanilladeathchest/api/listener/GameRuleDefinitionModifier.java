11116aefd36502b9ed6fea6302dc7fd230fc7375
TheRandomLabs <therandomlabsinc@gmail.com>
2019-01-23 23:17:11 +1100

    Remove redundant file

--- src/main/java/com/therandomlabs/vanilladeathchest/api/listener/GameRuleDefinitionModifier.java
+++
@@ -1,8 +0,0 @@
-package com.therandomlabs.vanilladeathchest.api.listener;
-
-import java.util.Map;
-import net.minecraft.world.GameRules;
-
-public interface GameRuleDefinitionModifier {
-	void modify(Map<String, GameRules.ValueDefinition> definitions);
-}

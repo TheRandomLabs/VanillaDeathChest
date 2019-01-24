52375d1de93557b30750de32010a241c3b87fd37
TheRandomLabs <therandomlabsinc@gmail.com>
2019-01-24 16:54:32 +1100

    Location search

--- src/main/java/com/therandomlabs/vanilladeathchest/util/DeathChestLocationFinder.java
+++ src/main/java/com/therandomlabs/vanilladeathchest/util/DeathChestLocationFinder.java
@@ -6,7 +6,6 @@ import java.util.List;
 import com.google.common.collect.ImmutableList;
 import com.therandomlabs.vanilladeathchest.VanillaDeathChest;
 import com.therandomlabs.vanilladeathchest.config.VDCConfig;
-import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.BlockItemUseContext;
@@ -119,9 +118,6 @@ public class DeathChestLocationFinder {
 			return false;
 		}
 
-		final IBlockState state = world.getBlockState(pos);
-		final IBlockState state2 = world.getBlockState(pos.up());
-
 		final BlockItemUseContext context = new BlockItemUseContext(new ItemUseContext(
 				player,
 				ItemStack.EMPTY,
@@ -132,13 +128,24 @@ public class DeathChestLocationFinder {
 				0.0F
 		));
 
-		if((state.isAir() || state.isReplaceable(context)) &&
-				(state2.isAir() || state2.isReplaceable(context))) {
+		if(isReplaceable(world, pos, context) && isReplaceable(world, pos.up(), context)) {
 			return isNotChest(world, pos.north()) && isNotChest(world, pos.east()) &&
 					isNotChest(world, pos.south()) && isNotChest(world, pos.west());
 		}
 
 		return false;
+	}
+
+	private static boolean isReplaceable(World world, BlockPos pos, BlockItemUseContext context) {
+		if(!VanillaDeathChest.CUBIC_CHUNKS_LOADED) {
+			final int y = pos.getY();
+
+			if(y < 1 || y > world.getActualHeight()) {
+				return false;
+			}
+		}
+
+		return world.getBlockState(pos).isReplaceable(context);
 	}
 
 	private static boolean isNotChest(World world, BlockPos pos) {
